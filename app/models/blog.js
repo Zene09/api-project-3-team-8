8ca2f51f127150c7a46d6a11401953b85fd48464
DRG104 <ph_kyle104@yahoo.com>
2022-08-03 14:18:31 -0500

    Added blog blogSchema, small virtual for testing on front end

---
+++ app/models/blog.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+// import commentSchema here
+
+const { Schema, model } = mongoose
+
+const blogSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		body: {
+			type: String,
+			required: true,
+		},
+        // subdoc comments
+        // comments: [commentSchema],
+		owner: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+		},
+	},
+	{
+		timestamps: true,
+		toObject: { virtuals: true },
+        toJSON: { virtuals: true }
+	}
+)
+
+blogSchema.virtual('blogInfo').get(function () {
+    return `${this.title} by ${this.owner}.`
+})
+
+module.exports = model('Blog', blogSchema)
